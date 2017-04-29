ce604d82472390bbeff7d68b54115dc3e38078ab
Niels Abels <n.abels@coolblue.nl>
2017-04-29 19:29:45 +0200

    Updated comments describing Osherove kata.

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -2,15 +2,18 @@
 
 namespace StringCalculator
 {
+    // http://osherove.com/tdd-kata-1/
     /*  Try not to read ahead.
         Do one task at a time. The trick is to learn to work incrementally.
         Make sure you only test for correct inputs. there is no need to test for invalid inputs for this kata */
 
-    //Create a simple String calculator with a method int Add(string numbers)
-    //The method can take 0, 1 or 2 numbers, and will return their sum(for an empty string it will return 0) for example “” or “1” or “1,2”
-    //Start with the simplest test case of an empty string and move to 1 and two numbers
-    //Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
-    //Remember to refactor after each passing test
+    // Create a simple String calculator with a method int Add(string numbers)
+    // [X] The method can take 0, 1 or 2 numbers, and will return their sum(for an empty string it will return 0) for example “” or “1” or “1,2”
+    // Start with the simplest test case of an empty string and move to 1 and two numbers
+    // Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
+    // Remember to refactor after each passing test
+
+    // [ ] Allow the Add method to handle an unknown amount of numbers
 
     public class StringCalculator
     {
